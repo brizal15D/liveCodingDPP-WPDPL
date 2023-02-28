068db712d95fdf7b7222e80de5ffe98d413b2a3b
Muhammad Rizal <89308171+rizal15D@users.noreply.github.com>
2023-02-28 15:42:32 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
           <a class="nav-link active" aria-current="page" href="#carouselExampleCaptions">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link active" href="#list">list</a>
+          <a class="nav-link active" href="#list">List</a>
         </li>
       </ul>
     </div>
